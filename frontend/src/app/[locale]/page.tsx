9261598f1f4f83ae88d59455e9cf0aa169b7960c
Fatih Egriboz <egriboz@gmail.com>
2023-10-18 10:48:59 +0300

    swiper carousel add

--- frontend/src/app/[locale]/page.tsx
+++ frontend/src/app/[locale]/page.tsx
@@ -1,5 +1,11 @@
-import { Homepage, MetadataProps, HomeProps, ContentImage } from "@/utils/types";
+import {
+  Homepage,
+  MetadataProps,
+  HomeProps,
+  ContentImage,
+} from "@/utils/types";
 import Container from "@/components/ui/Container";
+import HomeCarousel from "@/components/HomeCarousel";
 import Link from "next/link";
 import Image from "next/image";
 import { fetchData, API_URL, SERVER_IP } from "@/utils/api";
@@ -8,7 +14,7 @@ import { getTranslator } from "next-intl/server";
 import { Button } from "@/components/ui/button";
 import { Mail } from "lucide-react";
 import { Loader2 } from "lucide-react";
-import { DEFAULT_OG_IMAGE_URL } from '@/lib/config';
+import { DEFAULT_OG_IMAGE_URL } from "@/lib/config";
 
 import { Metadata, ResolvingMetadata } from "next";
 
@@ -27,11 +33,14 @@ export async function generateMetadata(
   const posts = await getHomepage();
   const firstPost = posts[0];
 
-  const firstImageUrl = firstPost.images && firstPost.images[0] ? firstPost.images[0].image : DEFAULT_OG_IMAGE_URL;
+  const firstImageUrl =
+    firstPost.images && firstPost.images[0]
+      ? firstPost.images[0].image
+      : DEFAULT_OG_IMAGE_URL;
 
   const description = `${firstPost.pageinfo} - ${t("sitedescription")}`;
   const title = `${firstPost.title} | ${t("sitename")}`;
-  
+
   return {
     title: title,
     description: description,
@@ -48,67 +57,84 @@ export default async function Posts({ params: { locale } }: HomeProps) {
   const homepage = posts[0];
   const t = await getTranslator(locale, "Globals");
   return (
-    <Container className="p-10 mt-16" id="content">
-      <div className="grid grid-cols-3 gap-4 mt-8">
-        {homepage.images &&
-          homepage.images.map((image: ContentImage) => (
-            <picture key={image.id}>
-              <Image
-                src={image.image}
-                priority={true}
-                width={500}
-                height={300}
-                alt={image.alt_text}
-                className="bg-slate-500"
-              />
-            </picture>
-          ))}
-      </div>
+    <>
+      <Container size="fluid">
+        <HomeCarousel homepage={homepage} />
+      </Container>
+      <Container className="p-10 mt-16" id="content">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          {homepage.images &&
+            homepage.images.map((image: ContentImage) => (
+              <picture key={image.id}>
+                <Image
+                  src={image.image}
+                  priority={true}
+                  width={500}
+                  height={300}
+                  alt={image.alt_text}
+                  className="bg-slate-500"
+                />
+              </picture>
+            ))}
+        </div>
+        <h1>{homepage.title}</h1>
+        {/* <div className="flex flex-wrap gap-4 p-4 my-16 rounded bg-slate-300"> */}
+        <div className="flex-wrap hidden gap-4 p-4 my-16 rounded bg-slate-300">
+          <Button className="">{t("button")}</Button>
+          <Button className="" variant="secondary">
+            {t("button")}
+          </Button>
+          <Button className="" variant="destructive">
+            {t("button")}
+          </Button>
+          <Button className="" variant="outline">
+            {t("button")}
+          </Button>
+          <Button className="" variant="ghost">
+            {t("button")}
+          </Button>
+          <Button className="" variant="link">
+            {t("button")}
+          </Button>
+          <Button>
+            <Mail className="w-4 h-4 mr-2" /> {t("button")}
+          </Button>
+          <Button disabled>
+            <Loader2 className="w-4 h-4 mr-2 animate-spin" />
+            {t("wait")}
+          </Button>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: homepage.content }} />
+        <hr className="h-0.5 my-10 bg-indigo-900" />
+        <h2 className="mt-16">{t("posts")}</h2>
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          {homepage.posts &&
+            homepage.posts.map((post) => (
+              <div key={post.id}>
+                <Link
+                  href={`/post/${post.slug}`}
+                  className="flex flex-col gap-3"
+                >
+                  <h5>{post.title}</h5>
+                  <picture>
+                    <Image
+                      src={post.image ? post.image : "/placeholder.jpg"}
+                      width={500}
+                      height={250}
+                      alt={post.title}
+                      className="bg-slate-500"
+                    />
+                  </picture>
 
-      <h1>{homepage.title}</h1>
-
-      {/* <div className="flex flex-wrap gap-4 p-4 my-16 rounded bg-slate-300"> */}
-      <div className="flex-wrap hidden gap-4 p-4 my-16 rounded bg-slate-300">
-        <Button className="" >{t("button")}</Button>
-        <Button className="" variant="secondary">{t("button")}</Button>
-        <Button className="" variant="destructive">{t("button")}</Button>
-        <Button className="" variant="outline">{t("button")}</Button>
-        <Button className="" variant="ghost">{t("button")}</Button>
-        <Button className="" variant="link">{t("button")}</Button>
-        <Button>
-          <Mail className="w-4 h-4 mr-2" /> {t("button")}
-        </Button>
-        <Button disabled>
-          <Loader2 className="w-4 h-4 mr-2 animate-spin" />
-          {t("wait")}
-        </Button>
-      </div>
-
-      <div dangerouslySetInnerHTML={{ __html: homepage.content }} />
-
-      <hr className="h-0.5 my-10 bg-indigo-900" />
-      <h2 className="mt-16">{t("posts")}</h2>
-      <div className="grid grid-cols-3 gap-4 mt-8">
-        {homepage.posts &&
-          homepage.posts.map((post) => (
-            <div key={post.id}>
-              <Link href={`/post/${post.slug}`} className="flex flex-col gap-3">
-                <h5>{post.title}</h5>
-                <picture>
-                  <Image
-                    src={post.image ? post.image : "/placeholder.jpg"}
-                    width={500}
-                    height={250}
-                    alt={post.title}
-                    className="bg-slate-500"
-                  />
-                </picture>
-                
-                <div dangerouslySetInnerHTML={{ __html: post.pageinfo }} />
-              </Link>
-            </div>
-          ))}
-      </div>
-    </Container>
+                  <div dangerouslySetInnerHTML={{ __html: post.pageinfo }} />
+                </Link>
+              </div>
+            ))}
+        </div>
+      </Container>
+      <Container>
+        <HomeCarousel homepage={homepage} />
+      </Container>
+    </>
   );
 }
---
+++ frontend/src/components/HomeCarousel.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { ContentImage } from "@/utils/types";
+import Image from "next/image";
+// Import Swiper
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+// import Swiper required modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+export default function App({ homepage }: any) {
+  return (
+    <Swiper
+      centeredSlides={true}
+      autoplay={{
+        delay: 5500,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Autoplay, Pagination, Navigation]}
+      className="h-[300px] md:h-[400px] lg:h-[550px] bg-slate-300"
+      // onSwiper={(swiper) => console.log(swiper)}
+      // onSlideChange={() => console.log("slide change")}
+    >
+      {homepage.images &&
+        homepage.images.map((image: ContentImage) => (
+          <SwiperSlide key={image.id}>
+            <picture className="block relative w-full h-full">
+              <Image
+                fill
+                src={image.image}
+                priority={true}
+                alt={image.alt_text}
+                className="w-full h-full object-cover object-center"
+              />
+            </picture>
+          </SwiperSlide>
+        ))}
+    </Swiper>
+  );
+}
