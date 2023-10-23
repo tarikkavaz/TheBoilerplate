1ed2d726b1e3e3e9614997a10c0d988c33ccdeb3
Tarik Kavaz <tarikkavaz@gmail.com>
2023-10-23 10:54:32 +0300

    sss

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -102,9 +102,6 @@ TEMPLATES = [
 # Static and media files
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
