68046a749975e68151951b62ae9d3af1d466044a
Tarik Kavaz <tarikkavaz@gmail.com>
2023-10-23 12:39:33 +0300

    static_root

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -102,7 +102,6 @@ TEMPLATES = [
 # Static and media files
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-# STATIC_ROOT = '/static_root/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
